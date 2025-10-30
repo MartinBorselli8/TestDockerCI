3c9bd7a4350f840e9d1533b336686fdb7e76049a
mborselli <mborselli@ext.sancorseguros.com>
2025-10-29 23:51:26 -0300

    add new url

--- TestDocker/Controllers/WeatherForecastController.cs
+++ TestDocker/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace TestDocker.Controllers
             })
             .ToArray();
         }
+        [HttpGet]
+        [Route("Test")]
+        public IEnumerable<WeatherForecast> test()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
